dddc6d68cd968cf4785bd55694aa93df8f6d0273
Advait "Conty" Contractor <advaitthepro@outlook.com>
2023-05-31 18:27:49 +0800

    Initial Commit

---
+++ Todoist/TodoistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoistApp.swift
+//  Todoist
+//
+//  Created by Milind Contractor on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
